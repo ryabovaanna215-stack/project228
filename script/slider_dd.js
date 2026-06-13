d02318114e6fac50f0358a43eb26da2656499611
Anna <ryabovaanna215@gmail.com>
2026-06-13 15:01:03 +0300

    Introduction slider d&d

---
+++ script/slider_dd.js
@@ -0,0 +1,35 @@
+const container_cards = document.querySelector(".cards");
+const card = document.querySelectorAll('.card');
+console.log('Найденные элементы:', card);
+
+let draggedCard;
+let startOrder = 1; 
+
+// ПЕРЕБИРАЕМ КАРТОЧКИ
+card.forEach(card => {
+    card.addEventListener("dragstart", (event) => {
+
+        const computedStyle = window.getComputedStyle(card); //чтение всех стилей css через браузер
+        startOrder = parseInt(computedStyle.order) //берет определённый стиль из css (то что в скобках) и превращает это в число (parseInt)
+
+        console.log(`Стартовый order взятой карточки: ${startOrder}`);
+        draggedCard = card;
+
+        event.dataTransfer.setDragImage(new Image(), 0, 0);
+    });
+});
+
+// ПЕРЕМЕЩЕНИЕ КАРТОЧКИ НАД ДРУГИМИ
+container_cards.addEventListener("dragover", (event) => {  
+    const currentCard = event.target.closest('.card');
+    if (currentCard && currentCard !== draggedCard) {
+        const currentComputedStyle = window.getComputedStyle(currentCard);
+        const currentOrder = parseInt(currentComputedStyle.order)
+        
+        if (startOrder !== currentOrder) {
+            draggedCard.style.order = currentOrder;
+            currentCard.style.order = startOrder;
+            startOrder = currentOrder;
+        }
+    }
+});
